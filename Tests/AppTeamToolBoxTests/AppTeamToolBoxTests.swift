2cc25331229846bf6752afac60c4f1e9e3925c77
Maximilian Nabokow <max.nabokow@gmail.com>
2021-07-01 13:54:54 -0400

    Cleanup

--- Tests/AppTeamToolBoxTests/AppTeamToolBoxTests.swift
+++ Tests/AppTeamToolBoxTests/AppTeamToolBoxTests.swift
@@ -1,11 +1,9 @@
-    import XCTest
-    @testable import AppTeamToolBox
+import XCTest
 
-    final class AppTeamToolBoxTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(AppTeamToolBox().text, "Hello, World!")
-        }
+final class AppTeamToolBoxTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
     }
+}
