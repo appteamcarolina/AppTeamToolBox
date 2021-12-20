97d90e361d5fffd7c8b666a07c7c5c18621eba4f
Max Nabokow <max.nabokow@gmail.com>
2021-12-20 15:52:25 +0100

    Added wait(for duration: TimeInterval) in Testing

---
+++ Sources/AppTeamToolBox/Testing.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//
+//
+//  Created by Max Nabokow on 12/20/21.
+//
+
+import XCTest
+
+extension XCTestCase {
+    func wait(for duration: TimeInterval) {
+        let waitExpectation = expectation(description: "Waiting")
+
+        let when = DispatchTime.now() + duration
+        DispatchQueue.main.asyncAfter(deadline: when) {
+            waitExpectation.fulfill()
+        }
+
+        waitForExpectations(timeout: duration + 0.5)
+    }
+}
