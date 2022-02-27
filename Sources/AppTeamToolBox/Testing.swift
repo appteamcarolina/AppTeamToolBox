8139e979c14f24575e133703e939743ca6666fdc
Max Nabokow <max.nabokow@gmail.com>
2022-02-26 22:52:34 -0500

    Remove Testing for now

--- Sources/AppTeamToolBox/Testing.swift
+++ Sources/AppTeamToolBox/Testing.swift
@@ -1,21 +1,21 @@
 //
-//  File.swift
+//  Testing.swift
 //
 //
 //  Created by Max Nabokow on 12/20/21.
 //
 
-import XCTest
-
-public extension XCTestCase {
-    func wait(for duration: TimeInterval) {
-        let waitExpectation = expectation(description: "Waiting")
-
-        let when = DispatchTime.now() + duration
-        DispatchQueue.main.asyncAfter(deadline: when) {
-            waitExpectation.fulfill()
-        }
-
-        waitForExpectations(timeout: duration + 0.5)
-    }
-}
+// import XCTest
+//
+// public extension XCTestCase {
+//    func wait(for duration: TimeInterval) {
+//        let waitExpectation = expectation(description: "Waiting")
+//
+//        let when = DispatchTime.now() + duration
+//        DispatchQueue.main.asyncAfter(deadline: when) {
+//            waitExpectation.fulfill()
+//        }
+//
+//        waitForExpectations(timeout: duration + 0.5)
+//    }
+// }
