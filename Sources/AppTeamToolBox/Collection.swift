b582122bf78ff5dd96e73404991e0f9f705f1841
Max Nabokow <max.nabokow@gmail.com>
2022-04-15 19:15:04 -0400

    Add reverse sort

--- Sources/AppTeamToolBox/Collection.swift
+++ Sources/AppTeamToolBox/Collection.swift
@@ -11,6 +11,19 @@ public extension Collection {
     /// Convenience function to avoid hard-to-read `!Collection.isEmpty` syntax
     /// - Returns: `True` if collection is not empty, `false` if it is.
     func isNotEmpty() -> Bool {
-        !self.isEmpty
+        !isEmpty
+    }
+
+    @available(iOS 15.0, *)
+    func sorted<Value: Comparable>(
+        by keyPath: KeyPath<Self.Element, Value>,
+        order: SortOrder = .forward
+    ) -> [Self.Element] {
+        switch order {
+        case .forward:
+            return sorted(by: { $0[keyPath: keyPath] < $1[keyPath: keyPath] })
+        case .reverse:
+            return sorted(by: { $0[keyPath: keyPath] > $1[keyPath: keyPath] })
+        }
     }
 }
