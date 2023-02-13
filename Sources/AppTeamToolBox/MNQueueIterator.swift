51613ef62e04fa345c6710db3d733c08f28df5bf
erayala15 <69863770+erayala15@users.noreply.github.com>
2023-02-13 03:23:34 -0500

    Fixed Centible undo bug crash (#1)
    * fixed Centible undo bug crash, changed fixIndexIfNeeded to return currIndex as nil instead of -1. Modified insert method so it doesn't break when given an empty array.
    
    * Add clipToRange
    
    * WIP
    
    * Finished transition to optional currIndex
    
    * Finished Optional currIndex migration
    
    ---------
    
    Co-authored-by: Max Nabokow <max.nabokow@gmail.com>

--- Sources/AppTeamToolBox/MNQueueIterator.swift
+++ Sources/AppTeamToolBox/MNQueueIterator.swift
@@ -9,20 +9,22 @@ import Foundation
 
 public struct MNQueueIterator<Element: Identifiable> {
     // MARK: PRIVATE
+
     private var items: [Element]
-    private var currIndex: Int
+    private var currIndex: Int?
 
     // MARK: PUBLIC
     
     public init(_ elements: [Element]) {
-        self.items = elements
+        items = elements
         currIndex = 0
     }
     
     public var elements: [Element] { items }
-    public var currentIndex: Int { currIndex }
+    public var currentIndex: Int? { currIndex }
 
     public var current: Element? {
+        guard let currIndex = currentIndex else { return nil }
         return items[safe: currIndex]
     }
 
@@ -30,50 +32,67 @@ public struct MNQueueIterator<Element: Identifiable> {
     public var isEmpty: Bool { items.isEmpty }
 
     public mutating func prev() {
-        currIndex -= 1
+        guard currIndex != nil else { return }
+        currIndex! -= 1
         fixIndexIfNeeded()
     }
 
     public mutating func next() {
-        currIndex += 1
+        guard currIndex != nil else { return }
+        currIndex! += 1
         fixIndexIfNeeded()
     }
 
     public mutating func insertAtCurrentIndex(_ element: Element) {
-        items.insert(element, at: currIndex)
+        if currIndex == nil { currIndex = 0 }
+        items.insert(element, at: currIndex!)
         fixIndexIfNeeded()
     }
     
     public mutating func setAllItems(to elements: [Element]) {
-        self.items = elements
+        items = elements
         fixIndexIfNeeded()
     }
 
     @discardableResult
     public mutating func removeCurrent() -> Element? {
-        guard !items.isEmpty else { return nil }
+        guard !items.isEmpty, let currIndex else { return nil }
 
-        defer { fixIndexIfNeeded() }
-        return items.remove(at: currIndex)
+        let removedItem = items.remove(at: currIndex)
+        
+        fixIndexIfNeeded() // Fix indices if currIndex is now invalid after the removal
+        
+        return removedItem
     }
     
     @discardableResult
-    public mutating func removeAll() -> [Element]? {
-        guard !items.isEmpty else { return nil }
-        currIndex = -1
-        defer { items.removeAll() }
-        return items
+    public mutating func removeAll() -> [Element] {
+        // Make sure items isn't empty already
+        guard !items.isEmpty else { return [] }
+        
+        let copy = items // Keep a copy of the items in the array to return later
+        
+        items.removeAll()
+        
+        fixIndexIfNeeded()
+        
+        return copy
     }
     
     // MARK: Private Helpers
 
     private mutating func fixIndexIfNeeded() {
-        guard !items.isEmpty else { currIndex = -1; return }
-
-        if currIndex >= items.endIndex {
-            currIndex = items.endIndex - 1
-        } else if currIndex < 0 {
-            currIndex = 0
-        }
+        // Only proceed if the index was valid before
+        // If it was invalid (nil), we don't know what to fix it to, so we keep it as is
+        guard currIndex != nil else { return }
+        
+        // Only proceed if there are items, else invalidate index since there are no elements to index into
+        guard !items.isEmpty else { currIndex = nil; return }
+        
+        // If we got here, there are definitely items and the index is non-nil
+        // So, we adjust the index to make sure its not out of bounds
+        
+        let validRange = 0 ... items.endIndex - 1
+        currIndex!.clipToRange(validRange)
     }
 }
