07a1bf751a7a115391858c10ca19cfec6976d84c
William Wang <wmwang52@gmail.com>
2024-02-05 17:37:22 -0500

    Test

--- Sources/AppTeamToolBox/MNQueueIterator.swift
+++ Sources/AppTeamToolBox/MNQueueIterator.swift
@@ -61,7 +61,7 @@ public struct MNQueueIterator<Element: Identifiable> {
     public mutating func reorderItems(to elements: [Element], index: Int) {
         if index == -1{
             
-            var idToIndexMap = [Int: Int]()
+            var idToIndexMap = [UUID: Int]()
             
             for (index, object) in elements.enumerated() {
                 idToIndexMap[object.id] = index
